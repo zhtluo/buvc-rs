11753eec18eecb8b4111598695ca72278c1a786b
zhtluo <zhtluo@gmail.com>
2024-10-27 00:18:23 -0400

    chore: Add test file

--- benches/update_witness_eth.rs
+++ benches/update_witness_eth.rs
@@ -13,24 +13,24 @@ fn benchmark_update_witnesses_batch(c: &mut Criterion) {
     group.sampling_mode(SamplingMode::Flat).sample_size(10); // Reduce sample size for long tests
 
     // Iterate over values of logn to get n from 2 to 100
-    for logn in 1..=18 {
+    for logn in 6..=12 {
         // log2(2) = 1, log2(100) ≈ 6.64, so iterate through logn from 1 to 7
         let n = 1 << logn; // n = 2^logn, e.g., n = 2, 4, 8, 16, ...
+        let m = 1 << 12;
 
         // Prepare test parameters using logn
-        let (_s, vc_p) = test_parameter(logn); // Pass logn directly to test_parameter
+        let (_s, vc_p) = test_parameter(12); // Pass logn directly to test_parameter
         let vc_c = VcContext::new(&vc_p, vc_p.logn);
 
         // Ensure vectors are consistent with n
-        let v: Vec<Fr> = (1..=n).map(|x| Fr::from(x as u64)).collect();
+        let v: Vec<Fr> = (1..=m).map(|x| Fr::from(x as u64)).collect();
         let gq = vc_c.build_commitment(&v).1; // This line will now have consistent input
 
-        let alpha_len = n / 2;
-        let alpha: Vec<usize> = (0..alpha_len).collect();
-        let beta: Vec<usize> = (alpha_len..n).collect();
+        let alpha: Vec<usize> = (0..n).collect();
+        let beta: Vec<usize> = (0..m).collect();
         let delta_value: Vec<Fr> = (1..=beta.len()).map(|x| Fr::from(x as u64)).collect();
 
-        group.bench_function(format!("update_witnesses_batch n = {}", n), |b| {
+        group.bench_function(format!("update_witnesses_batch_eth n = {}", n), |b| {
             b.iter(|| {
                 vc_c.update_witnesses_batch(&alpha, &gq, &beta, &delta_value);
             });
@@ -38,24 +38,24 @@ fn benchmark_update_witnesses_batch(c: &mut Criterion) {
     }
 
     // Iterate over values of logn to get n from 2 to 100
-    for logn in 1..=12 {
+    for logn in 6..=12 {
         // log2(2) = 1, log2(100) ≈ 6.64, so iterate through logn from 1 to 7
         let n = 1 << logn; // n = 2^logn, e.g., n = 2, 4, 8, 16, ...
+        let m = 1 << 12;
 
         // Prepare test parameters using logn
-        let (_s, vc_p) = test_parameter(logn); // Pass logn directly to test_parameter
+        let (_s, vc_p) = test_parameter(12); // Pass logn directly to test_parameter
         let vc_c = VcContext::new(&vc_p, vc_p.logn);
 
         // Ensure vectors are consistent with n
-        let v: Vec<Fr> = (1..=n).map(|x| Fr::from(x as u64)).collect();
+        let v: Vec<Fr> = (1..=m).map(|x| Fr::from(x as u64)).collect();
         let gq = vc_c.build_commitment(&v).1; // This line will now have consistent input
 
-        let alpha_len = n / 2;
-        let alpha: Vec<usize> = (0..alpha_len).collect();
-        let beta: Vec<usize> = (alpha_len..n).collect();
-        let delta_value: Vec<Fr> = (1..=alpha.len()).map(|x| Fr::from(x as u64)).collect();
+        let alpha: Vec<usize> = (0..n).collect();
+        let beta: Vec<usize> = (0..m).collect();
+        let delta_value: Vec<Fr> = (1..=beta.len()).map(|x| Fr::from(x as u64)).collect();
 
-        group.bench_function(format!("update_witness n = {}", n), |b| {
+        group.bench_function(format!("update_witness_eth n = {}", n), |b| {
             b.iter(|| {
                 for i in 0..alpha.len() {
                     for j in 0..beta.len() {
